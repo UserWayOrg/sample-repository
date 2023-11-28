0ed525171b1dca8f89de4b7b9566dd2148bd8cf3
Dmytro Fedkiv <dmytro.fedkiv@userway.org>
2023-11-28 17:14:42 +0100

    CA-497 Create OAuth authorization callback

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  // https://github.com/login/oauth/authorize?response_type=code&client_id=Iv1.4fd9bb1b2079c0be
+  .get(
+    "/github/oauth2/callback",
+    ({ query: { code } }) => `Your authorization code is ${code}`
+  )
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
